60220a519e1704b793c7e2e6f7b743fd209bd750
Felipe Alferez <alferezfelipe@gmail.com>
2024-08-12 16:31:02 -0500

    feat: add rpcException

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit
-} from "@nestjs/common";
+import { HttpStatus, Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 import { PrismaClient } from "@prisma/client";
 import { PaginationDto } from "../common/dto/pagination.dto";
+import { RpcException } from "@nestjs/microservices";
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -38,7 +34,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product) {
-      throw new NotFoundException(`Product with ID: ${id} dont found`);
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Product with ID: ${id} dont found`
+      });
     }
     return product;
   }
